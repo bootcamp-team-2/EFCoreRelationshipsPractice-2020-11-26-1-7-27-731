d68d2641bb10aff9db0e29f93fcc5505e435b85e
hockeyqqy <qqu@slb.com>
2020-11-26 15:30:11 +0800

    feat: add employeeentity to companyentity

--- EFCoreRelationshipsPractice/Entities/CompanyEntity.cs
+++ EFCoreRelationshipsPractice/Entities/CompanyEntity.cs
@@ -2,21 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EFCoreRelationshipsPractice.Dtos;
 
 namespace EFCoreRelationshipsPractice.Entities
 {
     public class CompanyEntity
     {
+        public CompanyEntity()
+        {
+        }
+
+        public CompanyEntity(CompanyDto companyDto)
+        {
+            this.Name = companyDto.Name;
+            this.Profile = new ProfileEntity(companyDto.Profile);
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
         public ProfileEntity Profile { get; set; }
+        public List<EmployeeEntity> Employees { get; set; }
     }
 
     public class ProfileEntity
     {
+        public ProfileEntity()
+        {
+        }
+
+        public ProfileEntity(ProfileDto profileDto)
+        {
+            this.CertId = profileDto.CertId;
+            this.RegisteredCapital = profileDto.RegisteredCapital;
+        }
+
         public int Id { get; set; }
         public int RegisteredCapital { get; set; }
         public string CertId { get; set; }
+        public ProfileEntity Entity { get; set; }
+    }
+
+    public class EmployeeEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
