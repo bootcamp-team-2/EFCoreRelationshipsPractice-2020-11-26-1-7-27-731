6daa6a749edb44eb471e5aba0b2df6f393307ce9
YYuan1414 <yuanyiren19900728@163.com>
2020-11-26 15:12:01 +0800

    feat: add profile to company entity

--- EFCoreRelationshipsPractice/Entity/CompanyEntity.cs
+++ EFCoreRelationshipsPractice/Entity/CompanyEntity.cs
@@ -11,5 +11,13 @@ namespace EFCoreRelationshipsPractice.Entity
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
+        public ProfileEntity Profile { get; set; }
+    }
+
+    public class ProfileEntity
+    {
+        public int Id { get; set; }
+        public int RegisteredCapital { get; set; }
+        public string CertId { get; set; }
     }
 }
